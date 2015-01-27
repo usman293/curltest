324cadcca1e1aecfb678327a9a7c491634420536
usman293 <usman.iqbal@nxb.com.pk>
2015-01-27 16:25:45 +0500

    fourth dsss commit

--- wp-content/themes/twentyfifteen/functions.php
+++ wp-content/themes/twentyfifteen/functions.php
@@ -34,6 +34,7 @@ function uploading_updates_on_git(){
 	//me new on git
 	//helloooooooooooooooo
 	//init.................
+	//fourth commit
 }
 add_action('init', 'uploading_updates_on_git');
 if ( ! isset( $content_width ) ) {
